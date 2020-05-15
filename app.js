5960a8fd33990f82377dbd62cfca5b282e3e3c88
Rowan Douglas <rowanandvera@gmail.com>
2020-05-15 11:53:54 -0500

    added button and centered it with a div will remove border later

--- app.js
+++ app.js
@@ -1,1 +1,11 @@
-alert("hello world");
+
+document.addEventListener("DOMContentLoaded", function () {
+    const div1 = document.createElement("div");
+    div1.className = "center";
+    const button1 = document.createElement("button");
+    button1.className = "button1";
+    const button1Text = document.createTextNode("Add Square");
+    button1.appendChild(button1Text);
+    div1.appendChild(button1)
+    document.body.appendChild(div1);
+});
